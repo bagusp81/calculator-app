269b664608f092a5033e9314e6a2075aa18ce918
Bagus Pranowo <61816313+bagusp81@users.noreply.github.com>
2020-03-13 06:05:07 +0700

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -1,33 +1,69 @@
-class Calculator
-{
-	static void Main(string[]args)
-	{
-		int a=10;
-		int b=6;
-		
-		Console.WriteLine("hasil {0} + {1} = {2}", a,b,penambahan(a,b));
-		Console.WriteLine("hasil {0} - {1} = {2}",a,b,pengurangan(a,b));
-		Console.WriteLine("hasil {0} x {1} = {2}", a,b,perkalian(a,b));
-		Console.WriteLine("hasil {0} : {1} = {2}",a,b,pembagian(a,b));
-		
-		Console.WriteLine("\nTekan sembarang. . .");
-		Console.ReadKey();
-	}
-	static int penambahan(int a, int b)
-	{
-		return a+b;
-	}
-	static int pengurangan(int a,int b)
-	{
-		return a-b;
-	}
-	static int perkalian(int a,int b)
-	{
-		return a*b;
-	}
-	static int pembagian(int a,int b)
-	{
-		return a/b;
-	}
-	
-}
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+            Console.WriteLine("1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian");
+            Console.Write("Pilihan anda: ");
+            int pilihan = Int32.Parse(Console.ReadLine());
+            int a, b;
+            
+
+            switch (pilihan)
+            {
+                case 1:
+                    Console.Write("input nilai a: ");
+                    a = Int32.Parse(Console.ReadLine());
+                    Console.Write("input nilai b: ");
+                    b = Int32.Parse(Console.ReadLine());
+                    Console.Write("\nnilai {0} + {1} = {2}", a, b, penjumlahan(a, b));
+                    break;
+                case 2:
+                    Console.Write("input nilai a: ");
+                    a = Int32.Parse(Console.ReadLine());
+                    Console.Write("input nilai b: ");
+                    b = Int32.Parse(Console.ReadLine());
+                    Console.Write("\nnilai {0} - {1} = {2}", a, b, pengurangan(a, b));
+                    break;
+                case 3:
+                    Console.Write("input nilai a: ");
+                    a = Int32.Parse(Console.ReadLine());
+                    Console.Write("input nilai b: ");
+                    b = Int32.Parse(Console.ReadLine());
+                    Console.Write("\nnilai {0} x {1} = {2}", a, b, perkalian(a, b));
+                    break;
+                case 4:
+                    Console.Write("input nilai a: ");
+                    a = Int32.Parse(Console.ReadLine());
+                    Console.Write("input nilai b: ");
+                    b = Int32.Parse(Console.ReadLine());
+                    Console.Write("\nnilai {0} : {1} = {2}", a, b, pembagian(a, b));
+                    break;
+                default:
+                    Console.Write("\nMaaf menu yang anda pilih tidak tersedia");
+                    break;
+            }
+            
+            
+        }
+        
+        static int penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int pembagian(int a, int b)
+        {
+            return a / b;
+        }
+    }
